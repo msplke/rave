f6598c543e9f1d0c691353a21425777efbb7b1a0
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-02-22 07:36:04 +0300

    Install & Configure Dependencies

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,6 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
     plugins: ["nativewind/babel", require.resolve("expo-router/babel")],
     presets: ["babel-preset-expo"],
   };
