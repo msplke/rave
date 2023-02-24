36ae84b3f6e2cd80f892906b40bf65eb6bab0e85
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-02-24 06:56:38 +0300

    Integrate Upstream Changes

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -1,11 +1,11 @@
-import type { AppRouter } from "@acme/api";
-import { transformer } from "@acme/api/transformer";
+import React from "react";
+import Constants from "expo-constants";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
-import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
-import Constants from "expo-constants";
-import React from "react";
+import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "@acme/api";
+import { transformer } from "@acme/api/transformer";
 
 /**
  * A set of typesafe hooks for consuming your API.
