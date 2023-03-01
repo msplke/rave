b68e704e3c8765d10dda825f6d152c0d90a722ec
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-03-01 06:51:02 +0300

    Configure React Native Paper & Inter Font

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -1,28 +1,52 @@
-import React from "react";
+import {
+  MD3LightTheme,
+  Provider as PaperProvider,
+  configureFonts,
+} from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 import { TRPCProvider } from "../src/utils/api";
 
+const fontConfig = {
+  fontFamily: "Inter-Regular",
+};
+
+const theme = {
+  ...MD3LightTheme,
+  fonts: configureFonts({ config: fontConfig }),
+};
+
 // This is the main layout of the app
 // It wraps your pages with the providers they need
 const RootLayout = () => {
+  const [fontsLoaded] = useFonts({
+    "Inter-Regular": require("../src/assets/fonts/Inter-Regular.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <TRPCProvider>
       <SafeAreaProvider>
-        {/*
+        <PaperProvider theme={theme}>
+          <StatusBar />
+          {/*           
           The Stack component displays the current page.
           It also allows you to configure your screens 
-        */}
-        <Stack
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: "#f472b6",
-            },
-          }}
-        />
-        <StatusBar />
+          */}
+          <Stack
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: "#f472b6",
+              },
+            }}
+          />
+        </PaperProvider>
       </SafeAreaProvider>
     </TRPCProvider>
   );
