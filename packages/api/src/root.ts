14e3fe69d2c67cef941a4691c465b796c36add62
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-04-09 07:17:35 +0300

    Delete Boilerplate, Create DB Schema & tRPC Routes

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,9 @@
-import { authRouter } from "./router/auth";
-import { postRouter } from "./router/post";
+import { communityRouter, eventRouter } from "./router";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
-  post: postRouter,
-  auth: authRouter,
+  community: communityRouter,
+  event: eventRouter,
 });
 
 // export type definition of API
