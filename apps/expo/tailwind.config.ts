aeeaf55fdd37100b6885aa70b3a55d3c64dc330f
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-05-25 04:58:53 +0300

    Update Tailwind Config

--- apps/expo/tailwind.config.ts
+++ apps/expo/tailwind.config.ts
@@ -3,6 +3,6 @@ import { type Config } from "tailwindcss";
 import baseConfig from "@acme/tailwind-config";
 
 export default {
+  content: ["./src/**/*.{ts,tsx}"],
   presets: [baseConfig],
-  content: ["./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
 } satisfies Config;
