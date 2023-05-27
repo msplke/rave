5b01ebbb0d11be9fad5d5971e67865e572c9d4e5
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-05-27 07:39:54 +0300

    Update Deps, Cache type-check, Expect ts-error

--- apps/expo/index.tsx
+++ apps/expo/index.tsx
@@ -4,6 +4,7 @@ import { ExpoRoot } from "expo-router";
 // Must be exported or Fast Refresh won't update the context
 export function App() {
   const ctx = require.context("./src/app");
+  // @ts-expect-error Type '__WebpackModuleApi.RequireContext' is not assignable to type 'import("/home/kibuchi/Development/my-projects/rave/node_modules/expo-router/build/types").RequireContext'.
   return <ExpoRoot context={ctx} />;
 }
 
